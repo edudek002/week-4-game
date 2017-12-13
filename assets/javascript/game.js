4a448f1a70ff798b74c58d7714b0be2703819bdc
Edyta <dudek002@hotmail.com>
2017-12-12 23:17:54 -0600

    solved repeated on click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,89 @@
 //create a large random number 19-120
 
-    var Min =19;
-    var Max =120;
-    var randomLg =0;
-    var min =1;
-    var max =12;
-    var randomSm = [];
-    var counter=0;
-    var gemValue=0;
+var Min =19;
+var Max =120;
+var randomLg =0;
+var min =1;
+var max =12;
+var randomSm = [];
+var counter=0;
+var win =0;
+var loss =0;
+var gemValue=0;
+
+   
+
+//Create a gem
+
+var imageGem=[$("<img>"),$("<img>"),$("<img>"),$("<img>")];
+
+for (var i = 0; i <4; i++) {      
+    imageGem[i].addClass("gem-image");
+    var color = ["assets/images/gemstoneBlue.jpg", "assets/images/gemstoneGreen.jpg", "assets/images/gemstoneRed.jpg", "assets/images/gemstoneYellow.jpg"];
+    imageGem[i].attr("src", color[i]);
+    $("#gems").append(imageGem[i]);
+};
 
 
-    
+function startGame() {
 
-    //Create a gem
-    
-    var imageGem=[$("<img>"),$("<img>"),$("<img>"),$("<img>")];
-    
-    for (var i = 0; i <4; i++) {      
+  // create a large random number 19-120 for different crystals
+  randomLg =0;
+  counter =0;
 
-        imageGem[i].addClass("gem-image");
-        var color = ["assets/images/gemstoneBlue.jpg", "assets/images/gemstoneGreen.jpg", "assets/images/gemstoneRed.jpg", "assets/images/gemstoneYellow.jpg"];
-        imageGem[i].attr("src", color[i]);
+  randomLg =Math.floor(Math.random() * (Max - Min + 1)) + Min;
+  console.log("Random Large = " + randomLg);
+  $("#random-numberLg").text(randomLg);
+  $("#total-number").text(this.counter);
+} 
 
-      };
+function roundComplete() {  
 
+  // create a small random number 1-12 for different crystals
+  randomSm = [0,0,0,0];
+  gemValue=0;
 
-    
-    
-    function startGame() {
+  for (var i = 0; i <4; i++) {
 
-      // create a large random number 19-120 for different crystals
-      randomLg =0;
-      counter =0;
+    randomSm[i] =Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(randomSm[i]);
+  
+    imageGem[i].attr("data-gemvalue", randomSm[i]);
+  };
+  
+}
 
-      randomLg =Math.floor(Math.random() * (Max - Min + 1)) + Min;
-      console.log("Random Large = " + randomLg);
-      $("#random-numberLg").text(randomLg);
-      $("#total-number").text(this.counter);
-    } 
+function registerEventListener() {
 
-    function roundComplete() {  
+  $(".gem-image").on("click", function() {
+  gemValue = ($(this).attr("data-gemvalue"));
 
-      // create a small random number 1-12 for different crystals
-      randomSm = [0,0,0,0];
-      gemValue=0;
+  console.log("After click gemvalue = " + gemValue);
+  alert("you guessed: " + gemValue);
+  gemValue = parseInt(gemValue);
+  counter += gemValue;
 
-      console.log("random small reset " + randomSm[3]);
-      console.log("gemvalue = " + gemValue);
-      
-      for (var i = 0; i <4; i++) {
+  $("#total-number").text(counter);
 
+  if (counter === randomLg) {
+    alert("You win!");
+    win++;
+    $("#wins").text(win);
+    //start new game
+    startGame();
+    roundComplete();
+  }
+  else if (counter >= randomLg) {
+    alert("You lose!!");
+    loss++; 
+    $("#losses").text(loss);
+    //start new game
+    startGame();
+    roundComplete();
+  }
 
-        randomSm[i] =Math.floor(Math.random() * (max - min + 1)) + min;
-        console.log(randomSm[i]);
-      
-        imageGem[i].attr("data-gemvalue", randomSm[i]);
-
-        $("#gems").append(imageGem[i]);
-      };
-
-
-      //$.each(randomSm, function (index, value))
-
-
-      $(".gem-image").on("click", function() {
-
-        gemValue = ($(this).attr("data-gemvalue"));
-        console.log("After click gemvalue = " + gemValue);
-        alert("you guessed: " + gemValue);
-        gemValue = parseInt(gemValue);
-        counter += gemValue;
-
-        $("#total-number").text(counter);
-
-        if (counter === randomLg) {
-          alert("You win!");
-          var win =0;
-          win++;
-          console.log("win " + win);
-          $("#wins").text(win);
-          //start new game
-          startGame();
-          roundComplete();
-        }
-        else if (counter >= randomLg) {
-          alert("You lose!!");
-          var loss =0;
-          loss++;
-          console.log("loss " + loss); 
-          $("#losses").text(loss);
-          //start new game
-          startGame();
-          roundComplete();
-        }
-
-      });
-
-    }
+  });
+}
 
 
     // MAIN PROCESS
@@ -108,3 +95,4 @@
 
     roundComplete();
 
+    registerEventListener();
